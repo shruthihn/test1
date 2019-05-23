557bd7f82f4f1759ca492ef37d02c33525d6e75f
Shruthi <shruthi.hns@gmail.com>
2019-05-23 08:31:48 +0530

    first commit

---
+++ test_Demo2.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_m1():
+    pass
+
+
+def test_m2():
+    pass
+
+
+def test_m3():
+    pass
+
+
+def test_m4():
+    e_r='abc logged in'
+    a_r='abc logged in'
+    assert e_r==a_r
